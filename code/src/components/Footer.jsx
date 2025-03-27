1832e04ee7a433605cb54543e8424a81d9617b80
Felipe Cesar Rodrigues <felpcsarodrigues@gmail.com>
2025-03-27 17:44:17 -0300

    first commit

---
+++ code/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+function Footer() {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright &copy; {currentYear}</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ code/src/components/Note.jsx
@@ -0,0 +1,14 @@
+import './Note.css';
+
+function Note() {
+    return (
+        <main className="container">
+            <div className="note">
+                <h1>This is the note title</h1>
+                <p>This is the note content</p>
+            </div>
+        </main>
+    );
+}
+
+export default Note;
