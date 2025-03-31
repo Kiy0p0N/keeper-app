c4c5e23c8c489ae98d0588cf2940e2a579613f61
Felipe Cesar Rodrigues <felpcsarodrigues@gmail.com>
2025-03-31 19:46:01 -0300

    mapping data to components

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,14 +1,29 @@
 import Header from './components/Header.jsx';
 import Note from './components/Note.jsx';
 import Footer from './components/Footer.jsx';
-import './App.css'
+import './App.css';
+import './components/Note.css';
+import notes from './notes.js';
+
+function createNote(note) {
+  return (
+    <Note
+      key={note.key}
+      id={note.key}
+      title={note.title}
+      content={note.content}
+    />
+  );
+}
 
 function App() {
   return (
     <>
       <Header />
 
-      <Note />
+      <main className="container">
+        {notes.map(createNote)}
+      </main>
 
       <Footer />
     </>
--- code/src/components/Note.jsx
+++ code/src/components/Note.jsx
@@ -1,13 +1,9 @@
-import './Note.css';
-
-function Note() {
+function Note(props) {
     return (
-        <main className="container">
-            <div className="note">
-                <h1>This is the note title</h1>
-                <p>This is the note content</p>
-            </div>
-        </main>
+        <div className="note">
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+        </div>
     );
 }
 
